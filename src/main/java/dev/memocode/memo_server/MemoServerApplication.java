78bfc20debda761082170be87d5f98df73a66e4d
leebuwon <bw1611@naver.com>
2024-03-04 19:18:50 +0900

    init commit

---
+++ src/main/java/dev/memocode/memo_server/MemoServerApplication.java
@@ -0,0 +1,13 @@
+package dev.memocode.memo_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/memocode/memo_server/MemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.memocode.memo_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
