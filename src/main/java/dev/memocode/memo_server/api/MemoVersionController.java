c0d965ff1c9725ff869c08b115a1085ab02d9913
leebuwon <bw1611@naver.com>
2024-03-07 20:39:23 +0900

    Feat: MemoVersion controller 생성

---
+++ src/main/java/dev/memocode/memo_server/api/MemoVersionController.java
@@ -0,0 +1,13 @@
+package dev.memocode.memo_server.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/memoversion")
+public class MemoVersionController {
+}
