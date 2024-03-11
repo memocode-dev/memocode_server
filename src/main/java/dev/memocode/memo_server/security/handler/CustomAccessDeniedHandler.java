869d8cbefec175315c7c1bd5af086a25959a2549
audgks5551 <audgsk5551@daum.net>
2024-03-11 13:09:45 +0900

    시큐리티 설정 수정2

--- src/main/java/dev/memocode/memo_server/security/handler/CustomAccessDeniedHandler.java
+++ src/main/java/dev/memocode/memo_server/security/handler/CustomAccessDeniedHandler.java
@@ -1,11 +1,11 @@
 package dev.memocode.memo_server.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.memocode.memo_server.exception.ErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import me.farmfarm.house_server.exception.ErrorResponse;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
--- src/main/java/dev/memocode/memo_server/security/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/dev/memocode/memo_server/security/handler/CustomAuthenticationEntryPoint.java
@@ -1,11 +1,11 @@
 package dev.memocode.memo_server.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.memocode.memo_server.exception.ErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import me.farmfarm.house_server.exception.ErrorResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
