0adff8a53be75977fcf5a5481b950e123667d491
leebuwon <bw1611@naver.com>
2024-03-25 12:59:54 +0900

    TestCode: Memo 생성 테스트 케이스 작성

--- src/main/java/dev/memocode/memo_server/domain/memo/dto/request/MemoCreateDTO.java
+++ src/main/java/dev/memocode/memo_server/domain/memo/dto/request/MemoCreateDTO.java
@@ -1,5 +1,6 @@
 package dev.memocode.memo_server.domain.memo.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +13,11 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemoCreateDTO {
+    @NotNull(message = "제목이 비어있습니다.")
     private String title;
+    @NotNull(message = "내용이 비어있습니다.")
     private String content;
+    @NotNull(message = "요약이 비어있습니다.")
     private String summary;
     private UUID authorId;
 }
