dff74bb04e22334464d71540e28204fc06c54742
audgks5551 <audgsk5551@daum.net>
2024-03-25 17:37:29 +0900

    Fix: 테스트 환경에서도 mysql, logstash, meilisearch가 동작할 수 있도록 구현

---
+++ src/test/java/dev/memocode/memo_server/base/BaseTest.java
@@ -0,0 +1,91 @@
+package dev.memocode.memo_server.base;
+
+import dev.memocode.memo_server.domain.author.entity.Author;
+import dev.memocode.memo_server.domain.author.repository.AuthorRepository;
+import dev.memocode.memo_server.testcontainer.LogstashContainer;
+import dev.memocode.memo_server.testcontainer.MeilisearchContainer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class BaseTest {
+    protected final static String MYSQL_DATABASE = "testdb";
+    protected final static String MYSQL_USERNAME = "test";
+    protected final static String MYSQL_PASSWORD = "test";
+
+    protected final static String MEILISEARCH_MASTER_KEY = "masterKey";
+
+    protected static final String PIPELINE_PATH = "/usr/share/logstash/pipeline/";
+    protected static final String CONFIG_PATH = "/usr/share/logstash/config/logstash.yml";
+    protected static final String DRIVER_PATH = "/opt/logstash/vendor/jar/jdbc/";
+
+    @Container
+    final static MySQLContainer<?> mysql =
+            new MySQLContainer<>(DockerImageName.parse("mysql:8.3.0"))
+                    .withDatabaseName(MYSQL_DATABASE)
+                    .withUsername(MYSQL_USERNAME)
+                    .withPassword(MYSQL_PASSWORD);
+
+    @Container
+    final static MeilisearchContainer meilisearch = new MeilisearchContainer()
+            .withMasterKey(MEILISEARCH_MASTER_KEY);
+
+    private final LogstashContainer logstash;
+
+    public BaseTest() {
+        mysql.start();
+        meilisearch.start();
+
+        logstash = new LogstashContainer()
+                .dependsOn(mysql, meilisearch)
+                .withEnv("MYSQL_PORT", String.valueOf(mysql.getMappedPort(3306)))
+                .withEnv("MYSQL_DATABASE", MYSQL_DATABASE)
+                .withEnv("MYSQL_USERNAME", MYSQL_USERNAME)
+                .withEnv("MYSQL_PASSWORD", MYSQL_PASSWORD)
+                .withEnv("MYSQL_PASSWORD", MEILISEARCH_MASTER_KEY)
+                .withEnv("MEILISEARCH_PORT", String.valueOf(meilisearch.getMappedPort(7700)))
+                .withClasspathResourceMapping(
+                        "docker/logstash/pipeline", PIPELINE_PATH, BindMode.READ_ONLY)
+                .withClasspathResourceMapping(
+                        "docker/logstash/config/logstash.yml", CONFIG_PATH, BindMode.READ_ONLY)
+                .withClasspathResourceMapping(
+                        "docker/logstash/drivers", DRIVER_PATH, BindMode.READ_ONLY);
+
+        logstash.start();
+    }
+
+    @DynamicPropertySource
+    static void mysqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.username", mysql::getUsername);
+    }
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    protected Author author;
+
+    @BeforeAll
+    void beforeAll() {
+        // 유저 생성
+        this.author = Author.builder()
+                .username("test1")
+                .nickname("test1")
+                .build();
+        authorRepository.save(this.author);
+    }
+}
