2bf2e64184209301a3abeeed6520d94ff3a2eb29
audgks5551 <audgsk5551@daum.net>
2024-04-25 19:02:13 +0900

    Fix: FindAllMemoComment_MemoCommentResult 어노테이션 변경2

--- application/memo/src/main/java/dev/memocode/application/memo/dto/result/FindAllMemoComment_MemoCommentResult.java
+++ application/memo/src/main/java/dev/memocode/application/memo/dto/result/FindAllMemoComment_MemoCommentResult.java
@@ -1,18 +1,26 @@
 package dev.memocode.application.memo.dto.result;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 @Data
 @Builder
+@JsonInclude(NON_NULL)
 public class FindAllMemoComment_MemoCommentResult {
     private UUID id;
     private String content;
     private FindAllMemoComment_UserResult user;
+    @JsonBackReference
     private List<FindAllMemoComment_MemoCommentResult> childMemoComments;
     private boolean deleted;
     private Instant createdAt;
