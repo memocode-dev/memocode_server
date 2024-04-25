37dc3233d2fa24b5fd927de838cf3a9619f66669
audgks5551 <audgsk5551@daum.net>
2024-04-25 18:38:45 +0900

    Fix: FindAllMemoComment_MemoCommentResult 어노테이션 변경

--- application/memo/src/main/java/dev/memocode/application/memo/dto/result/FindAllMemoComment_MemoCommentResult.java
+++ application/memo/src/main/java/dev/memocode/application/memo/dto/result/FindAllMemoComment_MemoCommentResult.java
@@ -1,6 +1,5 @@
 package dev.memocode.application.memo.dto.result;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,11 +7,8 @@ import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
-
 @Data
 @Builder
-@JsonInclude(NON_NULL)
 public class FindAllMemoComment_MemoCommentResult {
     private UUID id;
     private String content;
