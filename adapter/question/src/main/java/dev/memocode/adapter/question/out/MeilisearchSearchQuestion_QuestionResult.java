2cae43620ddf13c6147859f3086630949e32cf03
audgks5551 <audgsk5551@daum.net>
2024-04-29 01:15:39 +0900

    Fix: java.lang.NullPointerException: Cannot invoke 'java.lang.Boolean.booleanValue()' because the return value of 'dev.memocode.domain.question.immutable.ImmutableQuestion.getDeleted()' is null 에러 수정3

--- adapter/question/src/main/java/dev/memocode/adapter/question/out/MeilisearchSearchQuestion_QuestionResult.java
+++ adapter/question/src/main/java/dev/memocode/adapter/question/out/MeilisearchSearchQuestion_QuestionResult.java
@@ -1,5 +1,6 @@
 package dev.memocode.adapter.question.out;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,14 +15,24 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MeilisearchSearchQuestion_QuestionResult {
+    @JsonProperty(value = "id")
     private UUID id;
+    @JsonProperty(value = "title")
     private String title;
+    @JsonProperty(value = "content")
     private String content;
+    @JsonProperty(value = "_formatted")
     private MeilisearchSearchQuestion_QuestionResult _formatted;
+    @JsonProperty(value = "user")
     private MeilisearchSearchQuestion_UserResult user;
+    @JsonProperty(value = "tags")
     private Set<String> tags;
+    @JsonProperty(value = "createdAt")
     private Instant createdAt;
+    @JsonProperty(value = "updatedAt")
     private Instant updatedAt;
+    @JsonProperty(value = "deletedAt")
     private Instant deletedAt;
+    @JsonProperty(value = "deleted")
     private Boolean deleted;
 }
--- adapter/question/src/main/java/dev/memocode/adapter/question/out/MeilisearchSearchQuestion_UserResult.java
+++ adapter/question/src/main/java/dev/memocode/adapter/question/out/MeilisearchSearchQuestion_UserResult.java
@@ -1,5 +1,6 @@
 package dev.memocode.adapter.question.out;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,10 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MeilisearchSearchQuestion_UserResult {
+    @JsonProperty(value = "id")
     private UUID id;
+    @JsonProperty(value = "username")
     private String username;
+    @JsonProperty(value = "enabled")
     private Boolean enabled;
 }
