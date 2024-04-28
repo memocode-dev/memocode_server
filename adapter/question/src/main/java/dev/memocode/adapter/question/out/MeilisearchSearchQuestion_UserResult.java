18c7c745dfb64f2f7ca65ab2557c006443881817
audgks5551 <audgsk5551@daum.net>
2024-04-29 00:36:26 +0900

    Fix: com.fasterxml.jackson.databind.exc.InvalidDefinitionException: Cannot construct instance of  (no Creators, like default constructor, exist): cannot deserialize from Object value (no delegate- or property-based Creator) 에러 수정

--- adapter/question/src/main/java/dev/memocode/adapter/question/out/MeilisearchSearchQuestion_UserResult.java
+++ adapter/question/src/main/java/dev/memocode/adapter/question/out/MeilisearchSearchQuestion_UserResult.java
@@ -1,12 +1,16 @@
 package dev.memocode.adapter.question.out;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MeilisearchSearchQuestion_UserResult {
     private UUID id;
     private String username;
