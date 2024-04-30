4f1d6ef0e309ff1847c16a23ab7208a1d7ddf41a
audgks5551 <audgsk5551@daum.net>
2024-04-30 18:14:07 +0900

    Fix: question의 title 유효성검사 pattern 제거

--- domain/question/src/main/java/dev/memocode/domain/question/CreateQuestionDomainDTO.java
+++ domain/question/src/main/java/dev/memocode/domain/question/CreateQuestionDomainDTO.java
@@ -12,15 +12,13 @@ import java.util.Set;
 @Data
 @Builder
 public class CreateQuestionDomainDTO {
-    @NotNull(message = "VALIDATION_TITLE_NOT_NULL:title은 nulll일 수 없습니다.")
+    @NotNull(message = "VALIDATION_TITLE_NOT_NULL:title은 null일 수 없습니다.")
     @Size(max = 50, message = "VALIDATION_TITLE_SIZE:최대 50자까지 입력가능합니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_\\s~!@#$\t]*$",
-            message = "VALIDATION_TITLE_PATTERN:영소문자, 한글 그리고 특수문자(_, 공백, ~, !, @, #, $, 탭)만 입력가능합니다.")
     private String title;
 
-    @NotNull(message = "VALIDATION_CONTENT_NOT_NULL:content은 nulll일 수 없습니다.")
+    @NotNull(message = "VALIDATION_CONTENT_NOT_NULL:content은 null일 수 없습니다.")
     private String content;
 
-    @NotNull(message = "VALIDATION_TAGS_NOT_NULL:tags은 nulll일 수 없습니다.")
+    @NotNull(message = "VALIDATION_TAGS_NOT_NULL:tags은 null일 수 없습니다.")
     private Set<Tag> tags;
 }
