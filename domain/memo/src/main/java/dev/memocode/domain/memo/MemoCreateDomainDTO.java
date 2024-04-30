68d824318daddd0e688f9b43c76165280982c4a2
audgks5551 <audgsk5551@daum.net>
2024-04-30 18:12:57 +0900

    Fix: memo의 title, content 유효성검사 pattern 제거

--- domain/memo/src/main/java/dev/memocode/domain/memo/MemoCreateDomainDTO.java
+++ domain/memo/src/main/java/dev/memocode/domain/memo/MemoCreateDomainDTO.java
@@ -14,19 +14,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class MemoCreateDomainDTO {
-    @NotNull(message = "VALIDATION_TITLE_NOT_NULL:title은 nulll일 수 없습니다.")
+    @NotNull(message = "VALIDATION_TITLE_NOT_NULL:title은 null일 수 없습니다.")
     @Size(max = 50, message = "VALIDATION_TITLE_SIZE:최대 50자까지 입력가능합니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_\\s~!@#$\t]*$",
-            message = "VALIDATION_TITLE_PATTERN:영소문자, 한글 그리고 특수문자(_, 공백, ~, !, @, #, $, 탭)만 입력가능합니다.")
     private String title;
 
     @NotNull(message = "VALIDATION_CONTENT_NOT_NULL:content는 null일 수 없습니다.")
     private String content;
 
-    @NotNull(message = "VALIDATION_SUMMARY_NOT_NULL:summary는 nulll일 수 없습니다.")
+    @NotNull(message = "VALIDATION_SUMMARY_NOT_NULL:summary는 null일 수 없습니다.")
     @Size(max = 255, message = "VALIDATION_SUMMARY_SIZE:최대 255자를 입력할 수 있습니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_\\s~!@#$\t]*$",
-            message = "VALIDATION_SUMMARY_PATTERN:영소문자, 한글 그리고 특수문자(_, 공백, ~, !, @, #, $, 탭)만 입력가능합니다.")
     private String summary;
 
     @NotNull(message = "VALIDATION_USER_NOT_NULL:user는 null일 수 없습니다.")
