7996d90031561ecb85627b0a9ae98c8428e1f9a7
audgks5551 <audgsk5551@daum.net>
2024-04-30 15:04:04 +0900

    Cannot construct instance of  (although at least one Creator exists): cannot deserialize from Object value (no delegate- or property-based Creator) 문제 해결

--- adapter/question/src/main/java/dev/memocode/adapter/question/in/api/form/CreateQuestionForm.java
+++ adapter/question/src/main/java/dev/memocode/adapter/question/in/api/form/CreateQuestionForm.java
@@ -1,13 +1,17 @@
 package dev.memocode.adapter.question.in.api.form;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateQuestionForm {
     private String title;
     private String content;
