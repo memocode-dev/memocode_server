b78e71df608fe4f0165c7f9b8fe5db2cb28b17c8
audgks5551 <audgsk5551@daum.net>
2024-04-30 18:12:57 +0900

    Fix: memo의 title, content 유효성검사 pattern 제거

--- domain/memo/src/main/java/dev/memocode/domain/memo/MemoUpdateDomainDTO.java
+++ domain/memo/src/main/java/dev/memocode/domain/memo/MemoUpdateDomainDTO.java
@@ -11,13 +11,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public class MemoUpdateDomainDTO {
     @Size(max = 50, message = "VALIDATION_TITLE_SIZE:최대 50자까지 입력가능합니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_\\s~!@#$\t]*$",
-            message = "VALIDATION_TITLE_PATTERN:영소문자, 한글 그리고 특수문자(_, 공백, ~, !, @, #, $, 탭)만 입력가능합니다.")
     private String title;
 
     @Size(max = 255, message = "VALIDATION_SUMMARY_SIZE:최대 255자를 입력할 수 있습니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_\\s~!@#$\t]*$",
-            message = "VALIDATION_SUMMARY_PATTERN:영소문자, 한글 그리고 특수문자(_, 공백, ~, !, @, #, $, 탭)만 입력가능합니다.")
     private String summary;
 
     private String content;
