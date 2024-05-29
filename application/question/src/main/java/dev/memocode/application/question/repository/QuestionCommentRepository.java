1abe31ffbac69b5f1f0f8df3871233e164302162
duswnsxnxn <duswnsxnxn@gmail.com>
2024-05-29 14:28:08 +0900

    username으로 질문 댓글, 대댓글 조회 api 구현

--- application/question/src/main/java/dev/memocode/application/question/repository/QuestionCommentRepository.java
+++ application/question/src/main/java/dev/memocode/application/question/repository/QuestionCommentRepository.java
@@ -1,9 +1,13 @@
 package dev.memocode.application.question.repository;
 
 import dev.memocode.domain.question.QuestionComment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface QuestionCommentRepository extends JpaRepository<QuestionComment, UUID> {
+
+    Page<QuestionComment> findAllQuestionCommentByUserId(UUID userId, Pageable pageable);
 }
